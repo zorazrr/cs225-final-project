60a1fc0b5b4fd253a18c1fa8fdcb034525c442b4
ruoranz2 <ruoranz2@illinois.edu>
2022-05-01 04:30:27 +0000

    graph rendering california

---
+++ calipng.h
@@ -0,0 +1,14 @@
+#pragma once
+
+#include "graph.h"
+#include "dijkstra.h"
+#include "cs225/HSLAPixel.h"
+#include "cs225/PNG.h"
+
+using namespace std;
+using namespace cs225;
+
+PNG createCali(Graph g);
+std::pair<int, int> castPosition(double x, double y);
+
+void drawShortestPath(PNG &cali, Graph g, Dijkstra dijk, int dest);
